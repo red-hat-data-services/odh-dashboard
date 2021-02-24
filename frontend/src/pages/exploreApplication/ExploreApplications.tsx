72ff75dde56f4ce19a16a2bf23afbb7c31cc7df7
Jeffrey Phillips <jephilli@redhat.com>
2021-02-24 15:37:38 -0500

    Updates to allow 'coming soon' and show getting started side panel

--- frontend/src/pages/exploreApplication/ExploreApplications.tsx
+++ frontend/src/pages/exploreApplication/ExploreApplications.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Gallery } from '@patternfly/react-core';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerContentBody,
+  Gallery,
+  PageSection,
+} from '@patternfly/react-core';
 import { useWatchComponents } from '../../utilities/useWatchComponents';
 import OdhExploreCard from '../../components/OdhExploreCard';
 import ApplicationsPage from '../ApplicationsPage';
+import { ODHAppType } from '../../types';
+import GetStartedPanel from './GetStartedPanel';
 
 const description = `Add optional programs to your Red Hat OpenShift Data Science instance.`;
 
 const ExploreApplications: React.FC = () => {
   const { components, loaded, loadError } = useWatchComponents(false);
-  const [selectedComponent, setSelectedComponent] = React.useState<string>();
+  const [selectedComponent, setSelectedComponent] = React.useState<ODHAppType>();
   const isEmpty = !components || components.length === 0;
+
+  const updateSelection = React.useCallback(
+    (selection?: ODHAppType): void => {
+      if (!selection || selection === selectedComponent) {
+        setSelectedComponent(undefined);
+        return;
+      }
+      setSelectedComponent(selection);
+    },
+    [selectedComponent],
+  );
+
   return (
     <ApplicationsPage
       title="Explore"
@@ -19,18 +39,30 @@ const ExploreApplications: React.FC = () => {
       loadError={loadError}
     >
       {!isEmpty ? (
-        <Gallery className="odh-explore-apps__gallery" hasGutter>
-          {components
-            .sort((a, b) => a.spec.displayName.localeCompare(b.spec.displayName))
-            .map((c) => (
-              <OdhExploreCard
-                key={c.metadata.name}
-                odhApp={c}
-                isSelected={selectedComponent === c.metadata.name}
-                onSelect={() => setSelectedComponent(c.metadata.name)}
-              />
-            ))}
-        </Gallery>
+        <Drawer isExpanded={!!selectedComponent} isInline>
+          <DrawerContent
+            panelContent={
+              <GetStartedPanel onClose={() => updateSelection()} selectedApp={selectedComponent} />
+            }
+          >
+            <DrawerContentBody>
+              <PageSection>
+                <Gallery className="odh-explore-apps__gallery" hasGutter>
+                  {components
+                    .sort((a, b) => a.spec.displayName.localeCompare(b.spec.displayName))
+                    .map((c) => (
+                      <OdhExploreCard
+                        key={c.metadata.name}
+                        odhApp={c}
+                        isSelected={selectedComponent === c}
+                        onSelect={() => updateSelection(c)}
+                      />
+                    ))}
+                </Gallery>
+              </PageSection>
+            </DrawerContentBody>
+          </DrawerContent>
+        </Drawer>
       ) : null}
     </ApplicationsPage>
   );
