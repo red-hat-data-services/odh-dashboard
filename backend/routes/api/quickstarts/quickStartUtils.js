a070649b1b9c4c33f36d2d953325f521e3b57770
Jeffrey Phillips <jephilli@redhat.com>
2021-03-02 12:37:08 -0500

    Add flag to include mock data for demos

--- backend/routes/api/quickstarts/quickStartUtils.js
+++ backend/routes/api/quickstarts/quickStartUtils.js
@@ -19,16 +19,17 @@ const getInstalledQuickStarts = async (fastify) => {
     installedQuickStarts.push(...res.body.items);
   } catch (e) {
     fastify.log.error(e, 'failed to get quickstarts');
-    const error = createError(500, 'failed to get quickstarts');
-    error.explicitInternalServerError = true;
-    error.error = 'failed to get quickstarts';
-    error.message =
-      'Unable to load Kubeflow resources. Please ensure the Open Data Hub operator has been installed.';
-    throw error;
+    if (!constants.DEV_MODE && !constants.USE_MOCK_DATA) {
+      const error = createError(500, 'failed to get quickstarts');
+      error.explicitInternalServerError = true;
+      error.error = 'failed to get quickstarts';
+      error.message =
+        'Unable to load Kubeflow resources. Please ensure the Open Data Hub operator has been installed.';
+      throw error;
+    }
   }
 
-  // TODO: Remove
-  if (constants.DEV_MODE) {
+  if (constants.DEV_MODE || constants.USE_MOCK_DATA) {
     const mockQuickStarts = getMockQuickStarts();
     installedQuickStarts.push(...mockQuickStarts);
   }
--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -5,10 +5,13 @@ const IP = process.env.IP || '0.0.0.0';
 const LOG_LEVEL = process.env.FASTIFY_LOG_LEVEL || process.env.LOG_LEVEL || 'info';
 const DEV_MODE = process.env.APP_ENV === 'development';
 
+const USE_MOCK_DATA = DEV_MODE || process.env.USE_MOCK_DATA;
+
 module.exports = {
   PORT,
   IP,
   LOG_LEVEL,
   APP_ENV: process.env.APP_ENV,
   DEV_MODE,
+  USE_MOCK_DATA,
 };
